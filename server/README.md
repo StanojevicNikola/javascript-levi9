fc35951783e31c69c750f9a73fc3215ebdb4d510
StanojevicNikola <stanojevic.business@gmail.com>
2020-09-25 21:13:42 +0200

    code refactoring

--- server/README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-Project
-Playable game in pure JavaScript for Levi9 course
--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-  
-app.get('/', function(req, res) {
-    res.sendFile('/home/ov3rlord/Desktop/[JS] Drugi projekat/public/index.html');
-});
 
 app.use(express.static(path.join(__dirname, "public")));
+  
+app.get('/', function(req, res) {
+    res.sendFile('public/index.html');
+});
 
 PlayersController.registerRoutes(app);
 
